57d55d3af12bcde04790aae25c83440144e20fd5
David <damilear@outlook.com>
2022-06-03 01:23:20 -0400

    Better time complexity

--- python/155. Min Stack.py
+++ python/155. Min Stack.py
@@ -8,24 +8,50 @@ void push(int val) pushes the element val onto the stack.
 void pop() removes the element on the top of the stack.
 int top() gets the top element of the stack.
 int getMin() retrieves the minimum element in the stack.
+
+Example:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
 '''
 
 class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.M = [] #Track min val
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
+        if len(self.M) == 0:
+            self.stack.append(val)
+            self.M.append(val)
+        else:
+            self.stack.append(val)
+            self.M.append(min(val,self.M[-1]))
 
     def pop(self) -> None:
         self.stack.pop()
+        self.M.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.M[-1]
 
 
 # Your MinStack object will be instantiated and called as such:

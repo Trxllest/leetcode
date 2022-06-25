3bc18789ba1e9182848e21ad6b1f5588ae9249cc
David <damilear@outlook.com>
2022-06-25 00:54:41 -0400

    Solved

---
+++ python/704. Binary Search.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        len_nums = len(nums)
+        left = 0
+        right = len_nums-1
+        middle = (left + right)//2
+        while left <= right:
+            middle = (left + right)//2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                right = middle - 1
+        return -1
+    

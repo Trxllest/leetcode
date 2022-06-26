404135156317474e2002d89714738383874a9550
David <damilear@outlook.com>
2022-06-25 20:53:27 -0400

    Solved

---
+++ python/876. Middle of the Linked List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        [1] -> [1]
+        [1,2] -> [2]
+        """
+        slow,fast = head, head
+        while (fast != None and fast.next != None):
+            slow = slow.next
+            fast = fast.next.next
+        return slow
